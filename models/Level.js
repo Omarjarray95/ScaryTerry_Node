521e1d8f400e63898a39b6e207cd94d9eab29c5f
OmarJarray <Omar.jarray@esprit.tn>
2019-03-22 02:02:23 +0100

    Removed .idea + CRUD Examples

---
+++ models/Level.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var skillSchema = mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        description: {type: String, required: false},
+        level: {type: String, required: false,
+            enum: ["Not Applicable", "Fundamental", "Novice", "Intermediate", "Advanced", "Expert"]},
+        years: {type: Number, required: false}
+    });
+
+var skill = mongoose.model('Skill', skillSchema);
+
+module.exports = skill;
