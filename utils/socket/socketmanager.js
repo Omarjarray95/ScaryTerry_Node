0af9c19bd00b8f49b3ae4e782f5ab43dd050e817
Mejri Mohamed Ridha <mohamedridha.mejri@esprit.tn>
2019-03-29 06:08:19 +0100

    Socket.io serve same port as Express.

---
+++ utils/socket/socketmanager.js
@@ -0,0 +1,43 @@
+module.exports = function(server) {
+
+    const io = require('socket.io').listen(server)
+    var participants=[]
+
+    io.on('connection', function(socket) {
+        console.log("new one here , congrats !")
+      var client = {};
+      socket.on('new participant', function(name) {
+        // push to participant list
+        participants.push(name);
+        //update new client, this is used to track the client while disconnecting
+        client.name = name;
+        client.socket = socket;
+        console.log(new Date() + ' ' + client.name + ' connected');
+        // broadcast new user notification
+        socket.broadcast.emit('new participant', name);
+      });
+      socket.on('chat message', function(data) {
+        socket.broadcast.emit('chat message', data);
+      });
+      socket.on('typing', function(name) {
+        socket.broadcast.emit('typing', name);
+      });
+      socket.on('stoppedTyping', function(name) {
+        socket.broadcast.emit('stoppedTyping', name);
+      });
+      socket.on('removeTyping', function(name) {
+        socket.broadcast.emit('removeTyping', name);
+      });
+      socket.on('disconnect', function() {
+        /*if (client.name == undefined) {
+          // do nothing, client disconnected before joining chat
+        } else {
+          console.log(new Date() + ' ' + client.name + ' disconnected');
+          // remove from participants list
+          participants.splice(participants.indexOf(client.name), 1);
+          // broadcast to other participants
+          socket.broadcast.emit('disconnected', client.name);
+        }*/ console.log('Disconnected.')
+      });
+    });
+  }
