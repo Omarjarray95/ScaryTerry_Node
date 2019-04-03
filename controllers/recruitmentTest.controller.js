556007acd36b0961d437fed65220427251b489a3
Flavietta <saidi3mahdi@gmail.com>
2019-04-03 13:13:02 -0400

    judge0 api and job offer model

--- controllers/recruitmentTest.controller.js
+++ controllers/recruitmentTest.controller.js
@@ -3,6 +3,7 @@ var Quiz = require('../models/Quiz');
 var Code = require('../models/Code');
 var mongoose = require('mongoose');
 var needle = require('needle');
+var fs = require('fs');
 
 // TODO: After setting up the recruitment generator depending on the level
 // I have to make a level selection algorithm , that will calculate the 
@@ -42,6 +43,7 @@ var submitCode = (req, res, next) => {
     let _id = req.params.id;
     //TODO: here i have to make maybe a middleware that will check for the 
     // validation of this file 
+
     if (!code) {
         const error = new Error("The File is invalid");
         res.status(500).json(code);
@@ -95,24 +97,52 @@ var generateTest = (req, res, next) => {
 
 }
 
+
+// api Judge0.com 
 var testCode = (req, res, next) => {
-    const CLIENT_SECRET = "03859a3dd6e53b801e77fd50fb4e88d49a069aae";
-    const source = "print 'Hello World'";
-
-    var data = {
-
-        'client_secret': CLIENT_SECRET,
-        'async': 0,
-        'source': source,
-        'lang': "PYTHON",
-        'time_limit': 5,
-        'memory_limit': 262144,
-    }
-
-    needle.post('https://api.hackerearth.com/v3/code/run/', data, function (error, response, body) {
-        if (error)
+    //const CLIENT_SECRET = "03859a3dd6e53b801e77fd50fb4e88d49a069aae";
+    // const source = "printqeq 'Hello World'";
+    const URL_SUBMISSION = "https://api.judge0.com/submissions/?base64_encoded=false&wait=true";
+    const URL_LANGUAGE = "https://api.judge0.com/languages";
+    //TODO: Read the language from the database 
+    const language = "Go (1.9)";
+    //TODO: Read the file from the database
+    const file = fs.readFile('public/testcodes/code-2019-3-3.go', 'utf8', function (err, data) {
+        if (err)
             console.log(err);
-        console.log(body);
+        const source_code = data;
+        console.log(data);
+
+
+        //TODO: I can emit the call to get the language id , i already have them in my database 
+        needle.get(URL_LANGUAGE, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                body.forEach(element => {
+                    if (element.name === language) {
+
+                        const lang = element.id;
+                        var data = {
+                            "source_code": source_code,
+                            "language_id": lang,
+                            "stdin": "world"
+                        }
+
+                        needle.post(URL_SUBMISSION, data, function (error, response, body) {
+                            if (error)
+                                console.log(error);
+                            if (body.stderr)
+                                console.log("Error" + body.stderr);
+                            else
+                                console.log(body.stdout);
+                        });
+
+                    }
+                });
+            }
+        })
     });
 }
 
--- routes/testRecruitment.js
+++ routes/testRecruitment.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
-var test_controller = require('../controllers/recruitmentTest.controller');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../public/testcodes');
+        cb(null, 'public/testcodes');
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
+        //TODO: To Eliminate the case of duplicate name , i could do a name generator
+        const ext = file.originalname.split('.').pop();
+        const now = new Date(Date.now());
+        cb(null, file.fieldname + '-' + now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate() + '.' + ext);
     }
 });
+
+var upload = multer({ storage: storage });
+var test_controller = require('../controllers/recruitmentTest.controller');
+
 router.get('/generate', test_controller.generateTest);
 
-router.post('/', upload.single('code'), test_controller.add);
+router.post('/submit', upload.single('code'), test_controller.submitCode);
+
+router.post('/', test_controller.add);
 
 router.get('/:id', test_controller.get);
 
